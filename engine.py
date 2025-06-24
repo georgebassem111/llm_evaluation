9ead1344ba81e4b942ea8ecbf392c0aa7dbde604
georgebassem111 <gogobassem@gmail.com>
2025-06-24 22:14:16 +0300

    env

--- engine.py
+++ engine.py
@@ -1,1 +1,22 @@
 import torch
+import os
+import sys
+# from transformers import pipeline
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(f"Using device: {device}")
+
+
+print(sys.version)
+
+model_id = "meta-llama/Llama-3.2-1B"
+
+# pipe = pipeline(
+#     "text-generation", 
+#     model=model_id, 
+#     torch_dtype=torch.bfloat16, 
+#     device_map="auto"
+# )
+
+# pipe("The key to life is")
---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    print("Hello from llm-evaluation!")
+    import torch
+    print(torch.cuda.is_available())
+
+if __name__ == "__main__":
+    main()
